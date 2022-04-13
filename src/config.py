428d2488d08c0128e1ff6adc9524c0f064961740
Tobías Chavarría <70428147+tobias-chc@users.noreply.github.com>
2022-04-14 00:41:45 +0200

    Discord WEBHOOK_URL added in the config.py file.

--- src/config.py
+++ src/config.py
@@ -15,3 +15,11 @@ IMAGE_SIZE = (180, 180)
 BATCH_SIZE = 2
 BUFFER_SIZE = 32
 NUM_EPOCHS = 2
+
+# Discord notifications
+
+#Class
+#WEBHOOK_URL = "https://discord.com/api/webhooks/963737276649701416/1nqToKcI4G-GxHB1g72Iu-mWqf6PNFpfQdDfpFO7Wjq4qwZ_-N7a9hPPLpvjf_fJjGKc"
+
+#Personal
+WEBHOOK_URL = 'https://discord.com/api/webhooks/963930993843113994/OmmuPHpWgYdalAQQbzlcacoT6Sra4VX2wZpszfec5xEDOlVy3OZIO3c9vXyg5_3FIFSb'
