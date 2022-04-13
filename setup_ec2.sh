8c10dbf61d5c2f2e2deb3cdb76e1fad5f09bdd0e
Ubuntu <ubuntu@ip-172-31-5-184.ec2.internal>
2022-04-13 12:35:23 +0000

    setup_ec2.sh updated minor correction in load_data.py

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,7 +1,19 @@
 sudo apt-get update
 sudo apt-get install
-sudo apt install python3-pip
-sudo apt install python3-autopep8
+
+#Python env setup
+sudo apt install python3-pip -y
+sudo apt install python3-autopep8 -y
 
 pip3 install -r requirements.txt
 pip3 install tensorflow==2.8.0 --no-cache-dir
+
+#Docker installation
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt update
+sudo apt-get install docker-ce -y
+docker --version
+sudo systemctl start docker
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -45,7 +45,7 @@ class DataProcessor:
                 self.processed_directory, 'PetImages', folder_name)
             for fname in os.listdir(folder_path):
                 fpath = os.path.join(folder_path, fname)
-                print(fpath)
+                #print(fpath)
                 try:
                     fobj = open(fpath, "rb")
                     is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
