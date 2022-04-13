33b835f1d0a618521f8070d4df68dccdc067805f
Ubuntu <ubuntu@ip-172-31-5-184.ec2.internal>
2022-04-13 13:07:23 +0000

    setup_ec2.sh modified, Dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:20.04
+
+COPY . /Docker
+
+WORKDIR /Docker
+
+RUN apt-get update && apt-get install -y
+
+#Python env setup
+RUN apt install python3-pip -y
+RUN apt install python3-autopep8 -y
+
+RUN pip3 install -r requirements.txt
+RUN pip3 install tensorflow==2.8.0 --no-cache-dir
+
+CMD ["python3", "-m", "main.py"]
--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,13 +1,6 @@
 sudo apt-get update
 sudo apt-get install
 
-#Python env setup
-sudo apt install python3-pip -y
-sudo apt install python3-autopep8 -y
-
-pip3 install -r requirements.txt
-pip3 install tensorflow==2.8.0 --no-cache-dir
-
 #Docker installation
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
