e51ebea2751edca337e5b7fdac68f81e465665ff
Tobías Chavarría <70428147+tobias-chc@users.noreply.github.com>
2022-04-14 01:03:58 +0200

    Update setup_ec2.sh

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install
 
 #Docker installation
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
