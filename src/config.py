63ca869f56d89efa40bcddcd7a0c3feb82d568b8
Tobías Chavarría <70428147+tobias-chc@users.noreply.github.com>
2022-04-16 18:07:18 +0200

    Update config.py
    MODEL_PATH variable added, set NUM_EPOCHS = 1.

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@ RAW_DIRECTORY = os.path.sep.join([BASE_PATH, "raw"])
 INTERIM_DIRECTORY = os.path.sep.join([BASE_PATH, "interim"])
 PROCESSED_DIRECTORY = os.path.sep.join([BASE_PATH, "processed"])
 
+MODEL_PATH = os.path.sep.join(["models"])
+
 REMOTE_URL = 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip'
 
 
@@ -14,7 +16,7 @@ REMOTE_URL = 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8
 IMAGE_SIZE = (180, 180)
 BATCH_SIZE = 2
 BUFFER_SIZE = 32
-NUM_EPOCHS = 2
+NUM_EPOCHS = 1
 
 # Discord notifications
 
