18c8a53f6791601c3ee0e393ea6b888c2da9dbe2
Tobías Chavarría <70428147+tobias-chc@users.noreply.github.com>
2022-04-18 16:15:35 +0200

    Update config.py
    Edit the MODEL_PATH variable.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 
 # Path variables
 BASE_PATH = "data"
@@ -7,7 +7,10 @@ RAW_DIRECTORY = os.path.sep.join([BASE_PATH, "raw"])
 INTERIM_DIRECTORY = os.path.sep.join([BASE_PATH, "interim"])
 PROCESSED_DIRECTORY = os.path.sep.join([BASE_PATH, "processed"])
 
-MODEL_PATH = os.path.sep.join(["models"])
+#Use current datetime for distinct file name
+current_date = datetime.now().strftime("%m%d%Y-%H%M%S")
+
+MODEL_PATH = os.path.sep.join(["models", f"classifier_{current_datetime}.h5"])
 
 REMOTE_URL = 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip'
 
