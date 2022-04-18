a2ffb262e787017fd16c69a24c93e548c4c8c3d8
Tobías Chavarría <70428147+tobias-chc@users.noreply.github.com>
2022-04-18 16:22:00 +0200

    Update config.py
    MODEL_PATH format correction

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ INTERIM_DIRECTORY = os.path.sep.join([BASE_PATH, "interim"])
 PROCESSED_DIRECTORY = os.path.sep.join([BASE_PATH, "processed"])
 
 #Use current datetime for distinct file name
-current_date = datetime.now().strftime("%m%d%Y-%H%M%S")
+current_date = datetime.now().strftime("%m%d%Y")
 
 MODEL_PATH = os.path.sep.join(["models", f"classifier_{current_date}.h5"])
 
